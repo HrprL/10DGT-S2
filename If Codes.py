34e138f19d3ef3942cec9d0e6b8f95a0e7b5db94
HrprL <harper.lewis@kingsway.school.nz>
2024-09-18 10:25:11 +1200

    Add files via upload
    Wow

---
+++ If Codes.py
@@ -0,0 +1,13 @@
+number=2
+print(7+7)
+print(7^2)
+print("chat is this real")
+if number==1: 
+ print("*Yawn*")
+ print() #this makes a line break
+
+elif number== 2:
+ print("Fascinating\n")  
+ print("W")
+else: 
+ print("I'm Bored")
