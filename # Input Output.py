4ea712d6970042cac285fcbf9ac6de42aefc9326
HrprL <harper.lewis@kingsway.school.nz>
2024-09-20 14:42:35 +1200

    Add files via upload

---
+++ # Input Output.py
@@ -0,0 +1,10 @@
+#  Input Output 
+# Author: Harper Lewis
+#Date: 20/09/24
+username = input("What is your name ")
+
+fav_num = int(input("What is your favourite 1 digit number" )) 
+
+double= (fav_num/2)**2
+print(double)
+ 
