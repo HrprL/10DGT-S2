40c0af13226d70c9e671a4b78647e8074d1dc853
HrprL <harper.lewis@kingsway.school.nz>
2024-09-25 10:24:00 +1200

    Add files via upload

---
+++ # Loop work.py
@@ -0,0 +1,23 @@
+# Loop work
+# Date: 25/09/24
+# Author: Harper
+#Version 2
+
+# Get user input(Ask for name)
+# Ask for two numbers
+# Ask the user for their name
+
+name = input("What is your name?")
+print("Hello", name)
+age= int(input("What is your age"))
+print("Hello", age,"yr old", name)
+#Ask the user for two numbers 
+num_1= int(input("WHat is your favourite number?"))
+num_2= int(input("What is your least favourite number?"))
+sum = num_2 + num_1
+print("The numbers added together are", sum)
+
+for name_of_loop in range(10):
+
+ print("Ha")
+
